888d411038c4ac1bd58a17903d775d014a481ac6
Marcel Nowicki <0x414n@gmail.com>
2025-11-30 23:06:14 +0100

    Tweak compatibility condition for Math node

--- addon/compatibilty.py
+++ addon/compatibilty.py
@@ -5,6 +5,6 @@ BLENDER_VERSION = bpy.app.version
 
 def get_math_node_type() -> str:
     """Get correct compositing Math node type to use when adding nodes to node tree using Blender API."""
-    if BLENDER_VERSION >= (4, 4, 0):
+    if BLENDER_VERSION >= (4, 5, 0):
         return "ShaderNodeMath"
     return "CompositorNodeMath"
